b6b6d75c9536faa9fb02af868dcf4f2feb27fd76
William Santos <113859999+Williamwsm@users.noreply.github.com>
2024-06-25 19:43:42 -0300

    Update RegisterDTO.java
    importacao

--- src/main/java/com/example/demo/dto/RegisterDTO.java
+++ src/main/java/com/example/demo/dto/RegisterDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.dto;
 
 import com.example.demo.model.UserRole;
+import java.time.LocalDate;
 
 public record RegisterDTO (String login, String password, int role,  String nome, String fone, String email, LocalDate dataNascimento){
 }
