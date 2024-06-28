799af96930eed7ed0e44ee45de6fb1c2532291eb
will <113859999+Williamwsm@users.noreply.github.com>
2024-06-27 21:39:53 -0300

    findByCategorias

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,11 +4,13 @@ import com.example.demo.model.Categoria;
 import com.example.demo.model.Usuario;
 import com.example.demo.service.CategoriaService;
 import com.example.demo.service.UserService;
+import org.aspectj.weaver.loadtime.definition.LightXMLParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 //gerenciamento do usuário
@@ -32,13 +34,13 @@ public class UserController {
     }
 
     // Endpoint para adicionar categorias preferidas a um usuário existente.
-    @PostMapping("/{userId}/categorias")
+    @PostMapping("/{userId}/categorias") // quase certexa  q isso é um put ja  q taria atualizando as categorias preferidas de um usuario ja criado
     public ResponseEntity<Usuario> adicionarCategoriasPreferidas(
             @PathVariable Long userId,
             @RequestBody Set<Long> categoriaIds) {
 
         // Busca o usuário pelo ID
-        Usuario usuario = userService.findById(userId)
+        Usuario usuario = userService.findById(userId) /*todo esse codigo vai para o service pq o controller do deve char o metodo e nao ter a logica toda*/
                 .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado: " + userId));
 
         // Busca as categorias pelo IDs fornecidos
@@ -62,5 +64,18 @@ public class UserController {
     @GetMapping("/{userId}/categorias-preferidas")
     public Set<Categoria> buscarCategoriasPreferidas(@PathVariable Long userId) {
         return userService.consultarCategoriasPreferidas(userId);
+    }
+    @GetMapping("{/userid}/categorias")
+    public Categoria categoriaPreferida(@PathVariable Long userid){
+        return userService.findCategoriaById(userid);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Set<Categoria>> findCategoriaUsuario(@PathVariable Long id){
+        Usuario usuario = userService.findById(id).orElse(null);
+        if (usuario != null){
+            Set<Categoria> categorias = usuario.getCategoriasPreferidas();
+            return ResponseEntity.ok().body(categorias);
+        }
+        return null;
     }
 }
