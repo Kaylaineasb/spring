0decbd8bdd970e173f1d6f59797c77b314587df0
William Santos <113859999+Williamwsm@users.noreply.github.com>
2024-07-01 22:10:27 -0300

    Update NoticiaController.java

--- src/main/java/com/example/demo/controller/NoticiaController.java
+++ src/main/java/com/example/demo/controller/NoticiaController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Noticia;
 import com.example.demo.service.NoticiaService;
-import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
