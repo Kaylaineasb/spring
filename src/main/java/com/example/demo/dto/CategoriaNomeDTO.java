fcd831dd98f61efca8608fdc3ac46db9caf0afc8
Kaylaine Assunção <85030686+Kaylaineasb@users.noreply.github.com>
2024-08-04 09:31:53 -0300

    Update CategoriaNomeDTO.java

--- src/main/java/com/example/demo/dto/CategoriaNomeDTO.java
+++ src/main/java/com/example/demo/dto/CategoriaNomeDTO.java
@@ -1,29 +1,4 @@
 package com.example.demo.dto;
 
-public class CategoriaNomeDTO {
-    private Long id;
-    private String nome;
-
-    public CategoriaNomeDTO(Long id, String nome){
-        this.id = id;
-        this.nome = nome;
-    }
-
-    //Getters e Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+public record CategoriaNomeDTO (Long id,String nome){
 }
